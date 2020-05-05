a89ab97bbb94d72838f11e0cc0eabebad46369c0
sir-sa <mngumi44@gmail.com>
2020-05-05 10:35:01 -0700

    changes

--- resources/views/tickets/create.blade.php
+++ resources/views/tickets/create.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <main role="main" class="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
           <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
-            <h1 class="h2">Tickets</h1>
+            <h1 class="h2">Tickets page</h1>
             <div class="btn-toolbar mb-2 mb-md-0">
               <div class="btn-group mr-2">
                 <button class="btn btn-sm btn-outline-secondary">Share</button>
@@ -15,8 +15,8 @@
               </button>
             </div>
           </div>
- 
-           
+
+
           <form action="{{route('tickets.store')}}" method="POST">
             @csrf
 
